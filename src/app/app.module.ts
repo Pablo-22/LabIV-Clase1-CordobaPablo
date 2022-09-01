80ffb20bb8bdd3600155bf81ea12e2a82aaf44e1
Pablo Córdoba <pablo.cordoba@bromus.com.ar>
2022-09-01 19:37:40 -0300

    Creación de componentes - Ejercicio2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './Componentes/bienvenido/bienvenido.component';
+import { ErrorComponent } from './Componentes/error/error.component';
+import { LoginComponent } from './Vistas/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    ErrorComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
