2b2d842e088b4f632569df9184082457c157b789
Pablo Córdoba <pablo.cordoba@bromus.com.ar>
2022-09-01 19:28:13 -0300

    Ej1 - Funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Ej1-Cordoba-Pablo';
+export class AppComponent implements OnInit {
+	ageOne: number = 0;
+	ageTwo: number = 0;
+
+	sum: number = 0;
+	average: number = 0;
+
+
+
+	ngOnInit(): void {
+		
+	}
+
+	calculateValues(){
+		this.average = this.getAverage(this.ageOne, this.ageTwo);
+		this.sum = this.getSum(this.ageOne, this.ageTwo);
+	}
+
+	getAverage(...numbers:number[]){
+		let sum = this.getSum(...numbers)
+		return sum / numbers.length; 
+	}
+
+	getSum(...numbers:number[]){
+		return numbers.reduce((a, b) => a + b, 0);
+	}
+
+	resetInputs(){
+		this.ageOne = 0;
+		this.ageTwo = 0;
+		this.sum = 0;
+		this.average = 0;
+	}
 }
